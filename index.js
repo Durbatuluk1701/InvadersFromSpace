12488b4fbba4e647efbc03dedeff2d1258723e0a
Will Thomas <30wthomas@gmail.com>
2020-10-17 01:24:14 -0500

    It moves, albeit slowly

--- index.js
+++ index.js
@@ -4,6 +4,16 @@ let goober = new Image();
 goober.src = "Goober.png";
 let enemyArr = [];
 
+const Globals = {
+    playerSize: 20,
+    gooberSize: 20,
+    bulletSize: 10,
+    bulletColor: "lime",
+    tick: 500,
+    bulletCooldown: 200,
+    canFire: true
+}
+
 for (let i = 0; i < 200; i += 20) {
     for (let j = 0; j < 100; j += 20) {
         enemyArr.push([i, j]);
@@ -25,29 +35,64 @@ let Player = {
         drawPlayer();
     },
     fireBullet: () => {
-        shootBullet(Player.coords[0] + 10);
+        Bullets.bulletList.push([Player.coords[0] + 10, 0]);
     }
 }
 
-const Game = {
+let Game = {
+    gameInterval: 0,
+    direction: "right",
     opStack: [],
-    batchGooberMove: () => { },
-    batchPlayerMove: () => { },
-    batchPlayerShot: () => { }
+    gameTick: () => {
+        // Take Next OpStack
+        if (Game.opStack.length > 0) {
+            (Game.opStack.shift())();
+        }
+        // Check Hit Bullets
+        for (let i = 0; i < Bullets.bulletList.length; i++) {
+            if (checkHit(enemyArr, Bullets.bulletList[i], i)) {
+                break;
+            }
+        }
+        // Move Goobers
+        try {
+            moveGoobers();
+        }
+        // Catches error if the Goobers have gotten too close
+        catch (e) {
+            console.error(e);
+            stopGame(false);
+        }
+        // New Bullets
+        Bullets.handleBullets();
+        // Check Game Over
+        if (enemyArr.length === 0) {
+            stopGame(true);
+        }
+    },
+    startGame: () => {
+        Game.gameInterval = setInterval(Game.gameTick, Globals.tick);
+    }
 }
 
 const handleKeys = (e) => {
-    console.log(e.key);
     if (e.key.toLowerCase() === "d") {
+        console.log("Moving Right");
         Player.moveRight();
     }
     if (e.key.toLowerCase() === "a") {
-        console.log("moving left");
+        console.log("Moving Left");
         Player.moveLeft();
     }
     if (e.key === " ") {
         console.log("Firing Bullet");
-        Player.fireBullet();
+        if (Globals.canFire) {
+            Game.opStack.push(() => { Player.fireBullet() });
+            Globals.canFire = false;
+            setTimeout(() => {
+                Globals.canFire = true;
+            }, Globals.bulletCooldown);
+        }
     }
 }
 
@@ -59,20 +104,21 @@ const stopGame = (winner) => {
     }
     clearCanvas();
     document.removeEventListener("keypress", handleKeys);
+    clearInterval(Game.gameInterval);
 }
 
-const checkHit = (board, bullet) => {
+const checkHit = (board, bullet, index) => {
     for (let i = 0; i < board.length; i++) {
         let row = board[i];
+        console.log(Bullets.bulletList);
+        console.log(bullet, row);
         // Checks X within Row range
         if (bullet[0] <= row[0] + 20 && bullet[0] >= row[0]) {
             // Checks if Y within Row range
-            if (bullet[1] <= row[1] + 20 && bullet[1] >= row[1]) {
-                delete board[i];
-                enemyArr = board.filter((value) => {
-                    return Boolean(value);
-                })
+            if (canvas.height - 30 - bullet[1] <= row[1] + 20 && canvas.height - 30 - bullet[1] >= row[1]) {
+                board.splice(i, 1);
                 fillGoobers(board);
+                Bullets.bulletList.splice(index, 1);
                 return true;
             }
         }
@@ -83,51 +129,32 @@ const Bullets = {
     bulletObj: {
         "bulletUuid": "bulletNum"
     },
-    addBullet: (bulletUuid, bulletNum) => {
-        Bullets.bulletObj[`${bulletUuid}`] = bulletNum;
-    },
-    removeBullet: (bulletUuid) => {
-        let bulletNum = Bullets.bulletObj[`${bulletUuid}`];
-        delete Bullets.bulletObj[`${bulletUuid}`];
-        clearInterval(bulletNum);
+    bulletList: [],
+    handleBullets: () => {
+        Bullets.bulletList = Bullets.bulletList.filter((bulletArr) => {
+            return bulletArr[1] < canvas.height;
+        })
+        for (let i = 0; i < Bullets.bulletList.length; i++) {
+            let bullet = Bullets.bulletList[i];
+            context.beginPath();
+            context.strokeStyle = Globals.bulletColor;
+            context.moveTo(bullet[0], canvas.height - Globals.gooberSize - bullet[1]);
+            context.lineTo(bullet[0], canvas.height - Globals.bulletSize - Globals.gooberSize - bullet[1]);
+            context.stroke();
+            context.stroke();
+            context.stroke();
+            bullet[1] += 2.5;
+        }
     }
-}
-
-const shootBullet = (startXCoord) => {
-    // First path
-    let bulletDepth = 0;
-    let bulletUuid = Math.floor(Math.random() * 1000);
-    let bulletInterval = setInterval(() => {
-        console.log(bulletUuid);
-        context.beginPath();
-        context.strokeStyle = 'lime';
-        context.moveTo(startXCoord, canvas.height - 20 - bulletDepth);
-        context.lineTo(startXCoord, canvas.height - 30 - bulletDepth);
-        context.stroke();
-        bulletDepth += 5;
-        let hit = checkHit(enemyArr, [startXCoord, canvas.height - 20 - bulletDepth]);
-        if (hit) {
-            console.log("ending");
-            Bullets.removeBullet(bulletUuid);
-        }
-        if (enemyArr.length === 0) {
-            stopGame(true);
-        }
-    }, 100, bulletUuid);
-    Bullets.addBullet(bulletUuid, bulletInterval);
-    setTimeout(() => {
-        Bullets.removeBullet(bulletUuid);
-    }, 2500);
 }
 
 const fillGoobers = (enemyBoard) => {
     clearUpperCanvas();
     for (let i = 0; i < enemyBoard.length; i++) {
-        if (enemyBoard[i][1] + 20 >= canvas.height) {
-            stopGame(false);
-            break;
+        if (enemyBoard[i][1] + 2 * Globals.gooberSize >= canvas.height) {
+            throw "Game Over Error";
         }
-        context.drawImage(goober, enemyBoard[i][0], enemyBoard[i][1], 20, 20);
+        context.drawImage(goober, enemyBoard[i][0], enemyBoard[i][1], Globals.gooberSize, Globals.gooberSize);
     }
 }
 
@@ -150,54 +177,42 @@ const gooberChanges = (canvas, direction, currentBoard) => {
         if (direction === "right") {
             if (arr[0] + 20 >= canvas.width) {
                 direction = "left";
-                arrChanges[1] = arrChanges[1] + 5;
+                arrChanges[1] = arrChanges[1] + 2.5;
             }
         }
         else if (direction === "left") {
             if (arr[0] <= 0) {
                 direction = "right";
-                arrChanges[1] = arrChanges[1] + 5;
+                arrChanges[1] = arrChanges[1] + 2.5;
             }
         }
     });
     if (direction === "right") {
-        arrChanges[0] = arrChanges[0] + 5;
+        arrChanges[0] = arrChanges[0] + 2.5;
     } else {
-        arrChanges[0] = arrChanges[0] - 5;
+        arrChanges[0] = arrChanges[0] - 2.5;
     }
     return [arrChanges, direction];
 }
 
-const gooberDance = () => {
-    let direction = "right";
-    for (let i = 0; i < 100; i++) {
-        setTimeout(() => {
-            let [arrChanges, newDir] = gooberChanges(canvas, direction, enemyArr);
-            console.log(arrChanges, enemyArr);
-            direction = newDir;
-            enemyArr = enemyArr.map(arr => {
-                return [arr[0] + arrChanges[0], arr[1] + arrChanges[1]];
-            })
-            clearUpperCanvas();
-            fillGoobers(enemyArr);
-        }, i * 200);
-    }
-}
-
-// Fill Board Called Upon Image Load
-goober.addEventListener("load", () => {
+const moveGoobers = () => {
+    let [arrChanges, newDir] = gooberChanges(canvas, Game.direction, enemyArr);
+    Game.direction = newDir;
+    enemyArr = enemyArr.map(arr => {
+        return [arr[0] + arrChanges[0], arr[1] + arrChanges[1]];
+    })
+    clearUpperCanvas();
     fillGoobers(enemyArr);
-})
+}
 
 const startGame = () => {
     drawPlayer();
     document.addEventListener("keypress", handleKeys)
-    gooberDance();
+    Game.startGame();
 }
 
 const drawPlayer = () => {
     context.fillStyle = "red";
-    console.log(Player.coords);
     clearPlayerRow();
     context.fillRect(
         Player.coords[0],
